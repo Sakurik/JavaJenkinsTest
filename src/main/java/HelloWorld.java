39259c0b5e6b530aec285f5bd94accc36363079d
Golubev Nikita <golubev404@gmail.com>
2016-09-23 18:04:01 +0300

    error test - 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@
  */
 public class HelloWorld {
     public static void main(String ... args){
-        System.out.println("Test 6");;
+        System.out.println("Test 6");
+	throws Exception;
     }
 }
